57753138d1e8672e80aaf59c833e8e99a6483b5b
ryosuke.t <bmbr.318@icloud.com>
2026-04-01 21:18:10 +0900

    delete: unused code

--- sylva-app/src/main/java/io/github/ryosuke37/sylva/repository/DeletedPostRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.ryosuke37.sylva.repository;
-
-import io.github.ryosuke37.sylva.repository.entity.DeletedPostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  DeletedPostRepository extends JpaRepository<DeletedPostEntity, String> {
-}
--- sylva-app/src/main/java/io/github/ryosuke37/sylva/repository/DeletedUserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.ryosuke37.sylva.repository;
-
-import io.github.ryosuke37.sylva.repository.entity.DeletedUserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  DeletedUserRepository extends JpaRepository<DeletedUserEntity, String> {
-}
