2fac8eacb13bb4f2531f4779405814c254add836
ryosuke.t <bmbr.318@icloud.com>
2026-06-15 22:10:43 +0900

    add: meta info in html and commonize processing

--- sylva-app/src/main/java/io/github/ryosuke37/sylva/controller/SylvaController.java
+++ sylva-app/src/main/java/io/github/ryosuke37/sylva/controller/SylvaController.java
@@ -1,8 +1,5 @@
 package io.github.ryosuke37.sylva.controller;
 
-import io.github.ryosuke37.sylva.controller.dto.UserDto;
-import io.github.ryosuke37.sylva.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,29 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SylvaController {
 
-    private final AuthService authService;
-
-    @Autowired
-    SylvaController(
-            AuthService authService
-            ) {
-        this.authService = authService;
-    }
-
     @GetMapping
     public ModelAndView top() {
-        ModelAndView mav = new ModelAndView("/top");
-        UserDto loginUser = authService.getLoginUser();
-        mav.addObject("loginUser", loginUser);
-        return mav;
+        return new ModelAndView("/top");
     }
 
     @GetMapping("/tree/{id}")
     public ModelAndView tree(@PathVariable String id) {
         ModelAndView mav = new ModelAndView("/tree");
-        UserDto loginUser = authService.getLoginUser();
         mav.addObject("postId", id);
-        mav.addObject("loginUser", loginUser);
         return mav;
     }
 }
